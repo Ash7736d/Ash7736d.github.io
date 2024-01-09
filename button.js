7a719d76c455d71996ad2d85029eeedf047cf378
Ash <150865599+Ash7736d@users.noreply.github.com>
2024-01-09 08:04:47 +0800

    Update button.js

--- button.js
+++ button.js
@@ -1,6 +1,5 @@
-// button.js
 document.addEventListener("DOMContentLoaded", function () {
-  const button = document.getElementById("yourButtonId"); // Replace with your actual button ID
+  const button = document.getElementById("upcomingButton");
   button.addEventListener("click", function () {
     alert("Hello, this is a popup message!");
   });
