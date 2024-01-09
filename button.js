f2868a94ade1b0bfd0074442be6751b4d42dd3b3
Ash <150865599+Ash7736d@users.noreply.github.com>
2024-01-09 08:17:57 +0800

    Update button.js

--- button.js
+++ button.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const button = document.getElementById("upcomingButton");
   button.addEventListener("click", function () {
-    alert("Hello, this is a popup message!");
+    alert("WEBSITE IS STILL UNDER DEVELOPMENT⚙️");
   });
 });
